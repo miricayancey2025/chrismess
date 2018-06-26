054045bac5a7d2686ad1c268925353ae39ad0ffe
Mirica Yancey <mirica.yancey@valpo.edu>
2018-06-26 14:26:10 -0500

    Add Div List

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const form = document.querySelector('form')
 const revealFavorite = function(ev){
-      ev.preventDefault()
+    ev.preventDefault()
     const fav_input = document.querySelector('input').value
     const header = document.querySelector('.small')
+    const list = document.querySelector('#flicks')
     header.textContent = 'Favorite Chris is ' + fav_input
+    list.textContent += 'Favorite Chris movie is ' + fav_input + ' '
 }
 
 form.addEventListener('submit',revealFavorite)
