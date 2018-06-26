2bd51c0b8d0c3aeb480569753d92d268452f054c
Mirica Yancey <mirica.yancey@valpo.edu>
2018-06-26 14:44:34 -0500

    Space and Focus

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const form = document.querySelector('form')
+let i = 1
 const revealFavorite = function(ev){
     ev.preventDefault()
     const fav_input = document.querySelector('input').value
     const header = document.querySelector('.small')
     const list = document.querySelector('#flicks')
     header.textContent = 'Favorite Chris is ' + fav_input
-    list.textContent += 'Favorite Chris movie is ' + fav_input + ' '
+    list.innerHTML += `<p>${i}  ${fav_input}</p>`
+    i++
+    form.reset()
 }
 
 form.addEventListener('submit',revealFavorite)
