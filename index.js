f1ae51c3d0160879a26c4dd5bcf627cf0bc06291
Mirica Yancey <mirica.yancey@valpo.edu>
2018-06-26 14:58:05 -0500

    Ordered List

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const form = document.querySelector('form')
-let i = 1
 const revealFavorite = function(ev){
     ev.preventDefault()
     const fav_input = document.querySelector('input').value
     const header = document.querySelector('.small')
     const list = document.querySelector('#flicks')
     header.textContent = 'Favorite Chris is ' + fav_input
-    list.innerHTML += `<p>${i}  ${fav_input}</p>`
-    i++
+    list.innerHTML += `<li>${fav_input}</li>`
     form.reset()
 }
 
