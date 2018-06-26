e4b0f1cccd69af1ab7068dee21b35aff4213c0a4
Mirica Yancey <mirica.yancey@valpo.edu>
2018-06-26 15:24:33 -0500

    Create List Using Create Element and Append

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const form = document.querySelector('form')
-const revealFavorite = function(ev){
+const movieForm = function(ev){
     ev.preventDefault()
     const fav_input = document.querySelector('input').value
     const header = document.querySelector('.small')
     const list = document.querySelector('#flicks')
     header.textContent = 'Favorite Chris is ' + fav_input
-    list.innerHTML += `<li>${fav_input}</li>`
+    let item = document.createElement('li')
+    item.textContent = fav_input
+    list.appendChild(item)
     form.reset()
 }
 
-form.addEventListener('submit',revealFavorite)
+form.addEventListener('submit',moveiForm)
