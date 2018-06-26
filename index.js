a3a03238b354b7fea0c7b1fc74a04f6e6bdf49ad
Mirica Yancey <mirica.yancey@valpo.edu>
2018-06-25 23:13:08 -0500

    Homework Bonus Second Heading

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const button = document.querySelector("Button")
+const button = document.querySelector('Button')
+const button2 = document.querySelector('.button')
 const updateText = function () { 
-const heading = document.querySelector("h1")
-heading.textContent = 'New Heading!!'
+const heading = document.querySelector('h1')
+heading.textContent = 'IT\'s\ bout to be a party!!'
 }
+
+
+const updateHeading2 = function(){
+    const head = document.querySelector('.heading')
+    head.textContent = 'Ps. The lesser Chis is Chris Pine'
+}
+button2.addEventListener('click',updateHeading2)
 button.addEventListener('click', updateText)
