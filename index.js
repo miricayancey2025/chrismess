c4f6cb2381e0ee6db116ce609fa3d0024b694f7f
Mirica Yancey <mirica.yancey@valpo.edu>
2018-06-27 09:22:29 -0500

    Text Color Change, Span added

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const movieForm = function(ev){
     ev.preventDefault()
     const movie = document.querySelector('#movie_name').value // takes value of movie name
     const chris = document.querySelector('#chris_name').value // takes value of chris name
-    const movie_list = document.querySelector('#flicks') //finds div
-    let item = document.createElement('li') //creates element
-    item.textContent = movie + " starring " + chris // updates text to be added to element
-    movie_list.appendChild(item) //adds updated element to div
+    const movie_div = document.querySelector('#flicks') //finds div
+    const movie_list = document.createElement('li') //creates list element
+    const span = document.createElement('span') //creates span form movie title
+    const span2 = document.createElement('span')//creates span for chris name
+    span.setAttribute('class', 'movie_element') //sets attribute for movie title
+    span2.setAttribute('class', 'name_element')//sets attributes for chris name
+    span.innerHTML = movie //sets innerhtml of first span to movie
+    span2.innerHTML = " starring " + chris //sets innerhtml of second span to chris
+    movie_list.appendChild(span) //appends that to a movie list
+    movie_list.appendChild(span2)//appends that to a movie list
+    //by using the appends with the innerHTML should not be able to hack
+    movie_div.appendChild(movie_list) //appends that list item into the div
     form.reset()
 }
 
 form.addEventListener('submit',movieForm)
+//one li of two spans of different class names
